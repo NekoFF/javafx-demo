503b47946293a3229962aa636f30d9396f5050cd
NekoPC <mellnika221@gmail.com>
2025-12-29 15:22:14 +0100

    fix menu

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import javax.swing.*;
+
+
+public class Calculator {
+    public void Calc (){
+
+    String[] operation = {"+","-","*","/"};
+    
+    JOptionPane.showMessageDialog(null, "Das ist Calculator");
+    float z1 = 0;
+    float z2 = 0;
+
+    try{
+         z1 = Float.parseFloat(JOptionPane.showInputDialog(null, "erste Zahl"));
+         z2 = Float.parseFloat(JOptionPane.showInputDialog(null, "Zweite Zahl"));
+    }catch(Exception e){
+        JOptionPane.showMessageDialog(null, "Fehler");
+    }
+  
+    int resultop = JOptionPane.showOptionDialog(null, "Was willst du damit machen","Some", JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE,null, operation, operation[0]);
+  
+    float res = 0;
+    switch(resultop){
+        case 0: res = z1+z2;break;
+        case 1: res = z1-z2;break;
+        case 2: res = z1*z2;break;
+        case 3: res = z1/z2;break;
+    }
+    
+    JOptionPane.showMessageDialog(null, "Dein Result ist: " + res);
+
+
+    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,37 +1,20 @@
 package com.example;
-import java.util.*;
+
 import javax.swing.*;
 
 
 public class Main {
     public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-
-    String[] operation = {"+","-","*","/"};
-    
-    JOptionPane.showMessageDialog(null, "Das ist Calculator");
-    float z1 = 0;
-    float z2 = 0;
-
-    try{
-         z1 = Float.parseFloat(JOptionPane.showInputDialog(null, "erste Zahl"));
-         z2 = Float.parseFloat(JOptionPane.showInputDialog(null, "Zweite Zahl"));
-    }catch(Exception e){
-        JOptionPane.showMessageDialog(null, "Fehler");
-    }
   
-    int resultop = JOptionPane.showOptionDialog(null, "Was willst du damit machen","Some", JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE,null, operation, operation[0]);
-  
-    float res = 0;
-    switch(resultop){
-        case 0: res = z1+z2;break;
-        case 1: res = z1-z2;break;
-        case 2: res = z1*z2;break;
-        case 3: res = z1/z2;break;
-    }
+    Calculator cac = new Calculator();
     
-    JOptionPane.showMessageDialog(null, "Dein Result ist: " + res);
-
-    sc.close();
+    String menu[] = {"Calculator", "Exit"};
+    int vibor = JOptionPane.showOptionDialog(null, "","",JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, menu, menu[0]);
+    switch (vibor){
+        case 0: cac.Calc();
+        case 1: {JOptionPane.showMessageDialog(null, "POKA"); System.exit(0);}
+        case -1: System.exit(0);
+    }
+        
     }
 }
